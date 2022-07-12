cb97ccd62274b56313ec1d50ebc2c9fdd8b63d61
Surjith S M <surjithctly@gmail.com>
2022-07-12 14:44:55 +0530

    updates

--- studio/schemas/blockContent.js
+++ studio/schemas/blockContent.js
@@ -9,57 +9,82 @@
  *  }
  */
 export default {
-  title: 'Block Content',
-  name: 'blockContent',
-  type: 'array',
+  title: "Block Content",
+  name: "blockContent",
+  type: "array",
   of: [
     {
-      title: 'Block',
-      type: 'block',
+      title: "Block",
+      type: "block",
       // Styles let you set what your user can mark up blocks with. These
       // correspond with HTML tags, but you can set any title or value
       // you want and decide how you want to deal with it where you want to
       // use your content.
       styles: [
-        {title: 'Normal', value: 'normal'},
-        {title: 'H1', value: 'h1'},
-        {title: 'H2', value: 'h2'},
-        {title: 'H3', value: 'h3'},
-        {title: 'H4', value: 'h4'},
-        {title: 'Quote', value: 'blockquote'},
+        { title: "Normal", value: "normal" },
+        { title: "H2", value: "h2" },
+        { title: "H3", value: "h3" },
+        { title: "H4", value: "h4" },
+        { title: "Quote", value: "blockquote" }
       ],
-      lists: [{title: 'Bullet', value: 'bullet'}],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" }
+      ],
+
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
         // preference or highlighting by editors.
         decorators: [
-          {title: 'Strong', value: 'strong'},
-          {title: 'Emphasis', value: 'em'},
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
+          { title: "Code", value: "code" },
+          { title: "Underline", value: "underline" },
+          { title: "Strike", value: "strike-through" }
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
-            title: 'URL',
-            name: 'link',
-            type: 'object',
+            name: "internalLink",
+            type: "object",
+            title: "Internal link",
             fields: [
               {
-                title: 'URL',
-                name: 'href',
-                type: 'url',
-              },
-            ],
+                name: "reference",
+                type: "reference",
+                title: "Reference",
+                to: [
+                  { type: "docs" }
+                  // other types you may want to link to
+                ]
+              }
+            ]
           },
-        ],
-      },
+          {
+            title: "URL",
+            name: "link",
+            type: "object",
+            fields: [
+              {
+                title: "URL",
+                name: "href",
+                type: "url"
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      type: "code"
     },
     // You can add additional types here. Note that you can't use
     // primitive types such as 'string' and 'number' in the same array
     // as a block type.
     {
-      type: 'image',
-      options: {hotspot: true},
-    },
-  ],
-}
+      type: "image",
+      options: { hotspot: true }
+    }
+  ]
+};
